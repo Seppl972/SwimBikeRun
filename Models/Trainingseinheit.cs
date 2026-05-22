0a71ebb6104353d30722b8bbfe35a5ed61fd340f
Seppl972 <sebastianl973@gmx.de>
2026-05-22 14:46:35 +0200

    Grundlegende MVVM-Ordnerstruktur angelegt

---
+++ Models/Trainingseinheit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Später weitere Models ergänzen
+namespace SwimBikeRun.Models
+{
+    class Trainingseinheit
+    {
+        public int Id { get; set; }
+        public DateTime Datum { get; set; }
+        public string NameTrainingseinheit { get; set; }
+        public string Beschreibung { get; set; }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwimBikeRun.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}
